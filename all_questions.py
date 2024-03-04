2d4aad564e3edb9df8e32f7c14414b836d7da523
meghha <84433333+meghha@users.noreply.github.com>
2024-03-04 15:58:19 -0500

    Initial commit

---
+++ all_questions.py
@@ -0,0 +1,382 @@
+# Add import files
+import pickle
+
+
+
+# -----------------------------------------------------------
+def question1():
+    answers = {}
+
+    # type: bool (True/False)
+    answers["(a)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = "K-means clustering can be significantly influenced by outliers due to its reliance on mean-based centroid calculation, potentially leading to less reliable clustering results in the presence of outliers."
+
+    # type: bool (True/False)
+    answers["(b)"] = False
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = "Agglomerative hierarchical clustering procedures, while deterministic in their approach to merging clusters based on proximity, can still result in different clusterings depending on the specific linkage criteria or distance metric used"
+
+    # type: bool (True/False)
+    answers["(c)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(c) explain"] = "While it may not be true that K means is the most efficient clustering algorithm, it is indeed true that it fater than Agglomerative clustering. Agglomerative clustering is takes more time and memory than K means, more computationally intensive due to its quadratic or cubic time complexity, compared to the generally more efficient linear or slightly superlinear time complexity of K-means clustering"
+
+    # type: bool (True/False)
+    answers["(d)"] = 
+
+    # type: explanatory string (at least four words)
+    answers["(d) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(e)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(e) explain"] = "SSE is the sum of squared errors, which is the square of the distance between the data points in a cluster and its centroid. For more compact cohesive clusters, this value is low. That is, the more cohesion, the less is the SSE."
+
+    # type: bool (True/False)
+    answers["(f)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(f) explain"] = "SSB is the inter cluster distance, i.e., the disctance between the centroids of each cluster. As this increases, the seperation increases."
+
+    # type: bool (True/False)
+    answers["(g)"] = False
+
+    # type: explanatory string (at least four words)
+    answers["(g) explain"] = "This is false, because as the within cluster distance decreases, the inter cluster distance increases."
+
+    # type: bool (True/False)
+    answers["(h)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(h) explain"] = "The sum of SSE and SSB is equivalent to the variance of the data, which is a constant."
+
+    # type: bool (True/False)
+    answers["(i)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(i) explain"] = "True. SSE + SSB is a constant, and SSE is a representative of cohesion, while SSB is a representative for separation. As cohesion increases, i.e., SSE decreases, the SSB or separation increases."
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question2():
+    answers = {}
+
+    # type: bool (True/False)
+    answers["(a)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(b)"] = False
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(c)"] = True
+
+    # type: explanatory string (at least four words)
+    answers["(c) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question3():
+    answers = {}
+
+    # type: a string that evaluates to a float
+    answers["(a) SSE"] = "4 * (R^2)"
+
+    # type: a string that evaluates to a float
+    answers["(b) SSE"] = "4 * (a^2 + b^2  c^2)"
+
+    # type: a string that evaluates to a float
+    answers["(c) SSE"] = "10 * (R^2)"
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question4():
+    answers = {}
+
+    # type: int
+    answers["(a) Circle (a)"] = 0
+
+    # type: int
+    answers["(a) Circle (b)"] = 0
+
+    # type: int
+    answers["(a) Circle (c)"] = 0
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: int
+    answers["(b) Circle (a)"] = 0
+
+    # type: int
+    answers["(b) Circle (b)"] = 0
+
+    # type: int
+    answers["(b) Circle (c)"] = 0
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: int
+    answers["(c) Circle (a)"] = 0
+
+    # type: int
+    answers["(c) Circle (b)"] = 0
+
+    # type: int
+    answers["(c) Circle (c)"] = 0
+
+    # type: explanatory string (at least four words)
+    answers["(c) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question5():
+    answers = {}
+
+    # type: set
+    answers["(a)"] = set()
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: set
+    answers["(b)"] = set()
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question6():
+    answers = {}
+
+    # type: set
+    answers["(a) core"] = set()
+
+    # type: set
+    answers["(a) boundary"] = set()
+
+    # type: set
+    answers["(a) noise"] = set()
+
+    # type: set
+    answers["(b) cluster 1"] = set()
+
+    # type: set
+    answers["(b) cluster 2"] = set()
+
+    # type: set
+    answers["(b) cluster 3"] = set()
+
+    # type: set
+    answers["(b) cluster 4"] = set()
+
+    # type: set
+    answers["(c)-a core"] = set()
+
+    # type: set
+    answers["(c)-a boundary"] = set()
+
+    # type: set
+    answers["(c)-a noise"] = set()
+
+    # type: set
+    answers["(c)-b cluster 1"] = set()
+
+    # type: set
+    answers["(c)-b cluster 2"] = set()
+
+    # type: set
+    answers["(c)-b cluster 3"] = set()
+
+    # type: set
+    answers["(c)-b cluster 4"] = set()
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question7():
+    answers = {}
+
+    # type: string
+    answers["(a)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: string
+    answers["(b)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question8():
+    answers = {}
+
+    # type: string
+    answers["(a) Matrix 1"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain diag entries, Matrix 1"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain non-diag entries, Matrix 1"] = ""
+
+    # type: string
+    answers["(a) Matrix 2"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain diag entries, Matrix 2"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain non-diag entries, Matrix 2"] = ""
+
+    # type: string
+    answers["(a) Matrix 3"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain diag entries, Matrix 3"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain non-diag entries, Matrix 3"] = ""
+
+    # type: string
+    answers["(b) Row 1"] = ""
+
+    # type: string
+    answers["(b) Row 2"] = ""
+
+    # type: string
+    answers["(b) Row 3"] = ""
+
+    # type: string
+    answers["(b) Row 4"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 1 explain"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 2 explain"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 3 explain"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 4 explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question9():
+    answers = {}
+
+    # type: list
+    answers["(a)"] = []
+
+    # type: list
+    answers["(b)"] = []
+
+    # type: list
+    answers["(c)"] = []
+
+    # type: list
+    answers["(d)"] = []
+
+    # type: list
+    answers["(e)"] = []
+
+    # type: explanatory string (at least four words)
+    answers["(e) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question10():
+    answers = {}
+
+    # type: string
+    answers["(a) Figure (a)"] = ""
+
+    # type: string
+    answers["(a) Figure (b)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: string
+    answers["(b) Figure (a)"] = ""
+
+    # type: string
+    answers["(b) Figure (b)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: string
+    answers["(c)"] = ""
+
+    return answers
+
+# --------------------------------------------------------
+if __name__ == '__main__':
+    answers_dict = {}
+    answers_dict['question1'] = question1()
+    answers_dict['question2'] = question2()
+    answers_dict['question3'] = question3()
+    answers_dict['question4'] = question4()
+    answers_dict['question5'] = question5()
+    answers_dict['question6'] = question6()
+    answers_dict['question7'] = question7()
+    answers_dict['question8'] = question8()
+    answers_dict['question9'] = question9()
+    answers_dict['question10'] = question10()
+    print('end code')
+
+    with open('answers.pkl', 'wb') as f:
+        pickle.dump(answers_dict, f)
