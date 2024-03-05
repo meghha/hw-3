4ef2fd90c2caae83716d8e18e3c7331232f50160
meghha <84433333+meghha@users.noreply.github.com>
2024-03-04 19:43:02 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -257,28 +257,28 @@ def question8():
     answers["(a) Matrix 1"] = "Dataset Z"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain diag entries, Matrix 1"] = ""
+    answers["(a) explain diag entries, Matrix 1"] = "Since the diagonal entries are the distances between the points of a cluster, within its own cluster, and they are predominantly blue, it idicates a strong cohesion in the clusters. It means that the clusters are tightly packed and well differentiated."
 
     # type: explanatory string (at least four words)
-    answers["(a) explain non-diag entries, Matrix 1"] = ""
+    answers["(a) explain non-diag entries, Matrix 1"] = "Since there are varying colours in the cross diagonal entries of row 1 and 3, it is represented by clusters A and C. Similar observation for Cluster B and cluster D"
 
     # type: string
     answers["(a) Matrix 2"] = "Dataset X"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain diag entries, Matrix 2"] = ""
+    answers["(a) explain diag entries, Matrix 2"] = "The distinct and mainly blue qualities of diagonal entries are noticeable, setting them apart from the rest. This suggests a strong cohesion within clusters B and C."
 
     # type: explanatory string (at least four words)
-    answers["(a) explain non-diag entries, Matrix 2"] = ""
+    answers["(a) explain non-diag entries, Matrix 2"] = "Rows 1 and 4 exhibit diverse colors, implying varying distances from other clusters, with less distinct diagonal entries. Rows 2 and 3 demonstrate identical colors, suggesting equidistant relationships with two clusters and a greater separation from one cluster."
 
     # type: string
     answers["(a) Matrix 3"] = "Dataset Y"
 
     # type: explanatory string (at least four words)
-    answers["(a) explain diag entries, Matrix 3"] = ""
+    answers["(a) explain diag entries, Matrix 3"] = "Two diagonal entries, are distinctly clear in blue and signify robust cohesion within clusters B and C. This consistency suggests stronger intra-cluster connections within these groups."
 
     # type: explanatory string (at least four words)
-    answers["(a) explain non-diag entries, Matrix 3"] = ""
+    answers["(a) explain non-diag entries, Matrix 3"] = "In every row, two off-diagonal entries share matching colors, while one entry differs in color. This suggests that each cluster is relatively closer to two other clusters compared to the third."
 
     # type: string
     answers["(b) Row 1"] = "Cluster A"
@@ -293,16 +293,16 @@ def question8():
     answers["(b) Row 4"] = "Cluster D"
 
     # type: explanatory string (at least four words)
-    answers["(b) Row 1 explain"] = ""
+    answers["(b) Row 1 explain"] = "All off-diagonal entries exhibit distinct colors, indicating varied distances from other clusters. Since Cluster A has a less distinct diagonal entry, it suggests that there is a weaker cohesion."
 
     # type: explanatory string (at least four words)
-    answers["(b) Row 2 explain"] = ""
+    answers["(b) Row 2 explain"] = "Row 2 represents Cluster B, as two out of three off-diagonal entries share matching colors, indicating equidistant relationships, notably between B and A, and B and C. The clearer diagonal entry suggests strong cohesion within this cluster, contrasting with B's distance from D"
 
     # type: explanatory string (at least four words)
-    answers["(b) Row 3 explain"] = ""
+    answers["(b) Row 3 explain"] = "Row 3 denotes Cluster C, as evidenced by the clearer diagonal entry, indicating strong cohesion within this cluster. Two out of three off-diagonal entries show matching colors, emphasizing equidistant relationships, especially between C and B, and C and D. Notably, C is more distant from A in comparison."
 
     # type: explanatory string (at least four words)
-    answers["(b) Row 4 explain"] = ""
+    answers["(b) Row 4 explain"] = "The off-diagonal entries of Cluster D display different colors, hence it is most prominent. It indicates varying distances from other clusters. The less defined diagonal entry implies weaker cohesion within this cluster, confirming its classification as Cluster D"
 
     return answers
 
